b5ead1c2d57613d43fbce9c3f08e57ed8182fd3f
timos222 <timo.savva.ebay@gmail.com>
2020-07-18 10:10:41 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdf-table-builder",  # Replace with your own username
+    version="0.0.2",
+    author="Timotheos Savva",
+    author_email="timotheos.savva12@gmail.com",
+    description="A simple pdf table builder using the ReportLab Toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'reportlab',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
